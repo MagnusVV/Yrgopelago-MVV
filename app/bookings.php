fecaf0dd2e49acb63520ee7f4d6ee096f5837c98
MagnusVV <svartkub@msn.com>
2023-01-10 22:33:25 +0100

    Got the booking working again
    The "Trim" -function during "isset" of the transferCode removed double digits in the code's end. Trim is now removed.

--- app/bookings.php
+++ app/bookings.php
@@ -73,7 +73,7 @@ if (isset($_POST['arrivalDate']) && isset($_POST['departureDate']) && !empty($_P
 
                 if ($datesToCheck === $singleDates) {
                     // alert message if picked dates hits or overlaps already booked dates:
-                    $dateFailMessage = "You know, you can't just pick a date like you own the hotel, or something. We do have other guests in that room during, or part of, that period. Please try again.";
+                    $dateFailMessage = "You know, you can't just pick a date like you own the hotel. We do have other guests in that room during, or part of, that period. Please try again.";
 
                     failMessage($dateFailMessage);
                 }
@@ -96,7 +96,7 @@ $selectedExtraFeatures = [];
 if (isset($_POST['extrasFirst'])) {
     $extrasFirst = (int)htmlspecialchars($_POST['extrasFirst'], FILTER_SANITIZE_NUMBER_INT);
 
-    // this variable will put "1" as a boolean value in database:
+    // this variable will put "1" as a boolean value in database. The values have no inherent use, but provides an easy overview of selected extra features:
     $extrasFirstIsChoosen = 1;
     $selectedExtraFeatures[] = $extrasNames[$extrasFirst - 1];
 } else {
@@ -127,16 +127,15 @@ if (isset($_POST['totalCost'])) {
     $totalCost = (int)filter_var($_POST['totalCost'], FILTER_SANITIZE_NUMBER_INT);
 }
 
-
+// ---
 // If transferCode is valid, put all variables in the database. The table is chosen depending on $roomSelection value.
 
-//
 // transferCode: Check if code given is set, not empty and valid:
 if (isset($_POST['transferCode']) && !empty($_POST['transferCode'])) {
-    $transferCode = htmlspecialchars(trim($_POST['transferCode'], ENT_QUOTES));
+    $transferCode = htmlspecialchars($_POST['transferCode'], ENT_QUOTES);
+
 
     if (isValidUuid($transferCode)) {
-
         $client = new GuzzleHttp\Client();
         $options = [
             'form_params' => [
@@ -144,84 +143,81 @@ if (isset($_POST['transferCode']) && !empty($_POST['transferCode'])) {
             ]
         ];
 
+        // transferCode: Check if code is valid:
         try {
             $response = $client->post("https://www.yrgopelago.se/centralbank/transferCode", $options);
             $response = $response->getBody()->getContents();
             $response = json_decode($response, true);
-
-            // var_dump($response);
-
-            if (isset($response['transferCode']) && isset($response['amount']) && $response['amount'] >= $totalCost) {
-
-                $user = 'Magnus';
-
-                $getMonies = [
-                    'form_params' => [
-                        "user" => $user, "transferCode" => $transferCode
-                    ]
-                ];
-                try {
-                    $getPaidResponse = $client->post("https://www.yrgopelago.se/centralbank/deposit", $getMonies);
-                    $getPaidResponse = $getPaidResponse->getBody()->getContents();
-                    $getPaidResponse = json_decode($getPaidResponse, true);
-                } catch (\Exception $e) {
-                    return "Error occured!" . $e;
-                };
-                $dateBaseWriteTest = "yes";
-            } else {
-                // alert message if transferCode (payment) is invalid:
-                $codeFailMessage = "You should be more careful with money. Please go back and use a correct payment code";
-
-                failMessage($codeFailMessage);
-            };
         } catch (\Exception $e) {
             return "Error occured!" . $e;
         }
+
+        if (isset($response['transferCode']) && isset($response['amount'])) {
+            $user = 'Magnus';
+
+            $getMonies = [
+                'form_params' => [
+                    "user" => $user, "transferCode" => $transferCode
+                ]
+            ];
+
+            // transferCode: Payment is deposited at "centralbank":
+            try {
+                $getPaidResponse = $client->post("https://www.yrgopelago.se/centralbank/deposit", $getMonies);
+                $getPaidResponse = $getPaidResponse->getBody()->getContents();
+                $getPaidResponse = json_decode($getPaidResponse, true);
+            } catch (\Exception $e) {
+                return "Error occured!" . $e;
+            };
+
+            // ---
+            // successful payment: booking parameters is inserted in database:
+
+            //query modified by room no:
+            $bookingQuery = createBookingQuery($roomSelection);
+
+            $insertIntoDb = $hotelDatabase->prepare($bookingQuery);
+
+            $insertIntoDb->bindParam(':guest_name', $customer, PDO::PARAM_STR);
+            $insertIntoDb->bindParam(':payment_code', $transferCode, PDO::PARAM_STR);
+            $insertIntoDb->bindParam(':arrival_date', $arrivalDate, PDO::PARAM_STR);
+            $insertIntoDb->bindParam(':departure_date', $departureDate, PDO::PARAM_STR);
+            $insertIntoDb->bindParam(':extra_feature_1', $extrasFirstIsChoosen, PDO::PARAM_BOOL);
+            $insertIntoDb->bindParam(':extra_feature_2', $extrasSecondIsChoosen, PDO::PARAM_BOOL);
+            $insertIntoDb->bindParam(':extra_feature_3', $extrasThirdIsChoosen, PDO::PARAM_BOOL);
+            $insertIntoDb->bindParam(':total_cost', $totalCost, PDO::PARAM_INT);
+
+            $insertIntoDb->execute();
+
+            // ---
+            // JSON-response for succesful booking:
+
+            // the booking reponse before conversion:
+            $bookingResponse = [
+                'island' => 'Ilha das mil pragas',
+                'hotel' => 'Terminal Hoteleiro',
+                'Your room choice' => $roomNames[$roomSelection - 1],
+                'arrival_date' => $arrivalDate,
+                'departure_date' => $departureDate,
+                'total_cost' => $totalCost,
+                'stars' => 2,
+                'features' => $selectedExtraFeatures,
+                'additional_info' => 'We are very much looking forward to your stay here. And remember, if you survive this visit to Ilha das mil pragas, you can survive anything!',
+            ];
+
+            // the booking reponse being converted to .json:
+            $jsonbookingResponse = json_encode($bookingResponse);
+
+            header('Content-Type: application/json');
+
+            echo $jsonbookingResponse;
+        } else {
+            // alert message if transferCode (payment) is invalid:
+            $codeFailMessage = "Either you tried using an obsolete code, or you tried to pay LESS for MORE. Please go back and use a valid code, or perhaps book something closer to your wallet?";
+
+            failMessage($codeFailMessage);
+        }
     }
-}
-
-//
-
-if ($dateBaseWriteTest === 'yes') {
-
-    //query modified by room no:
-    $bookingQuery = createBookingQuery($roomSelection);
-
-    $insertIntoDb = $hotelDatabase->prepare($bookingQuery);
-
-    $insertIntoDb->bindParam(':guest_name', $customer, PDO::PARAM_STR);
-    $insertIntoDb->bindParam(':payment_code', $transferCode, PDO::PARAM_STR);
-    $insertIntoDb->bindParam(':arrival_date', $arrivalDate, PDO::PARAM_STR);
-    $insertIntoDb->bindParam(':departure_date', $departureDate, PDO::PARAM_STR);
-    $insertIntoDb->bindParam(':extra_feature_1', $extrasFirstIsChoosen, PDO::PARAM_BOOL);
-    $insertIntoDb->bindParam(':extra_feature_2', $extrasSecondIsChoosen, PDO::PARAM_BOOL);
-    $insertIntoDb->bindParam(':extra_feature_3', $extrasThirdIsChoosen, PDO::PARAM_BOOL);
-    $insertIntoDb->bindParam(':total_cost', $totalCost, PDO::PARAM_INT);
-
-    $insertIntoDb->execute();
 } else {
     echo 'Catastrophic booking error!';
 }
-
-// ---
-// JSON-response for succesful booking:
-
-// the booking reponse before conversion:
-$bookingResponse = [
-    'island' => 'Ilha das mil pragas',
-    'hotel' => 'Terminal Hoteleiro',
-    'Your room choice' => $roomNames[$roomSelection - 1],
-    'arrival_date' => $arrivalDate,
-    'departure_date' => $departureDate,
-    'total_cost' => $totalCost,
-    'stars' => 2,
-    'features' => $selectedExtraFeatures,
-    'additional_info' => 'We are very much looking forward to your stay here. And remember, if you survive this visit to Ilha das mil pragas, you can survive anything!',
-];
-
-// the booking reponse being converted to .json:
-$jsonbookingResponse = json_encode($bookingResponse);
-
-header('Content-Type: application/json');
-
-echo $jsonbookingResponse;
